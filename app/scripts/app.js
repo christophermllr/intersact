4c420e3323dfb74462c9be43da88a147aed9ac45
Letteer Lewis <letteer3@gmail.com>
2013-05-01 18:45:16 -0700

    arch

---
+++ app/scripts/app.js
@@ -0,0 +1,78 @@
+/**
+ app.js | 8.7.2012 | v1.0
+ USFoods Mobile Ordering
+ Copyright 2012. US Foods Inc.
+ @module Sales
+*/
+
+Clementine.add('intersact.app', function(exports) {
+  
+  // Declarations
+  
+  var IntersActApplication;
+  
+  // Dependencies
+  
+  var View = Clementine.View;
+  
+  // Declare services
+  var IntersActService = include('klm.services.intersact').IntersActService;
+
+  // Declare Controllers
+  var IntersActController = include('klm.controllers.intersact').IntersActController;
+
+  // Decare Repository
+  var IntersActRepository = include('klm.repositories.intersact').IntersActRepository;
+  
+  
+  // Controller Definitions
+  
+  IntersActApplication = Class.extend({
+    
+    /**
+     @class IntersActApplication
+     @extend Controller
+     @param {Object} config
+     */
+    initialize: function(config) {
+      
+      // services
+      this.services = {
+        'intersact': new IntersActService(config.path)
+      };
+      
+      // register views
+      View.register(config.views || [], this.render);      
+      this.rootController = null;
+      
+      // store globally
+      window.App = this;
+      
+    },
+    
+    render: function() {
+      var intersactRepository = new intersactRepository(localStorage);
+      this.root = new IntersActController(intersactRepository);
+      this.root.view.appendTo(document.body);
+      this.root.load();
+      $(document.body).removeClass('hidden');
+    },
+    
+    getService: function(name) {
+      
+      if (this.services.hasOwnProperty(name)) {
+        return this.services[name];
+      } else {
+        throw 'Service "' + name + '" not found';
+      }
+      
+    }
+    
+  });
+  
+  
+  // Exports
+  
+  exports.IntersActApplication = IntersActApplication;
+    
+}, ['klm.services.intersact', 'klm.controllers.intersact', 'klm.repositories.intersact']);
---
+++ app/scripts/controllers/intersact/intersact.js
@@ -0,0 +1,119 @@
+/**
+ sales-tools.js | 8.7.2012 | v1.0
+ USFoods Mobile Ordering
+ Copyright 2012. US Foods Inc.
+ @module Sales
+*/
+
+Clementine.add('klm.controllers.intersact', function(exports) {
+  
+  // Declarations
+  
+  var IntersActController;
+  
+  
+  // Dependencies
+    
+  var View = Clementine.View;
+  
+  var Controller                      = include('core.controllers.common').Controller;
+  var NavigationController            = include('core.controllers.navigation').NavigationController;
+  var ModalController                 = include('core.controllers.modal').ModalController;
+  var IntersActRepository             = include('klm.repositories.intersact').IntersActRepository;
+  
+  // Controller Definitions
+  
+  IntersActController = Controller.extend({
+    
+    /**
+     @class IntersActController
+     @extends Controller
+     @constructor
+     @param {IntersActRepository} intersactRepository
+     */
+    initialize: function(intersactRepository) {
+      
+      // call super
+      this._super();
+      
+      // store repository
+      this.intersactRepository = intersactRepository;
+       
+      // store state
+      this.selectedCustomer = null;
+      this.selectedDepartment = null;
+      
+      // create primary view
+      this.view = new View('intersact');
+      
+    },
+    
+    
+    // Configuration
+    
+    /**
+     @method getBindings
+     @return {Object} The map of event bindings keyed by name.
+     */
+    getBindings: function() {
+      return {
+        'back': { 'back': this.onBack }
+      };
+    },
+    
+    /**
+     @method getRoutes
+     @return {Object} The map of routes methods for the controller.
+     */
+    getRoutes: function() {
+      
+      function onHome(current) {
+                          
+      }
+      
+      function onList(current) {
+                      
+      }
+    
+      return {
+        'home': onHome,
+        'list': onList,
+      };
+      
+    },
+    
+    
+    // Public Methods
+    
+    /**
+     Loads the controller and binds all listeners.
+     @method load
+     */
+    load: function() {
+    
+      this.navigateTo('home');
+
+    },
+    
+    /**
+     Unloads the controller and removes all listeners.
+     @method unload
+     */
+    unload: function() {
+      
+      this._super();
+    
+    }
+    
+  });  
+  
+  // Exports
+  
+  exports.IntersActController        = IntersActController;
+    
+}, [
+  'core.controllers.common',
+  'core.controllers.navigation',
+  'core.controllers.modal',
+  'klm.repositories.intersact'
+]);
